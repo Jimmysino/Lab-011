4b5240c799618e77447b73d2005e6373411e5103
Jimmy Melendez <leitotapia07@gmail.com>
2026-06-20 17:04:58 -0500

    feat: Creacion de ejercicio y

---
+++ src/Bebida.java
@@ -0,0 +1,9 @@
+public abstract class Bebida {
+    String description="Bebida desconocida";
+
+    public abstract double costo();
+
+    public String getDescription() {
+        return description;
+    }
+}
