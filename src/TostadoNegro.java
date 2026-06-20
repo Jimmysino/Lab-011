5c14e2d3aedb8dbe55af0e83ffb6b070c5909e2d
Jimmy Melendez <leitotapia07@gmail.com>
2026-06-20 17:07:11 -0500

    feat: Creacion de TostadoNegro

---
+++ src/TostadoNegro.java
@@ -0,0 +1,12 @@
+public class TostadoNegro extends Bebida {
+
+
+    public TostadoNegro() {
+        description="Tostado Negro";
+    }
+
+    @Override
+    public double costo() {
+        return 0.99;
+    }
+}
