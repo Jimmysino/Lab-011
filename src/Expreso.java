c3e262b7c3009bcb9925a1bb6278674f75cb3db9
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:09:08 -0500

    fix: Cambio en Expreso para implementar HashMap

--- src/Expreso.java
+++ src/Expreso.java
@@ -1,10 +1,16 @@
+import java.util.HashMap;
+
 public class Expreso extends Bebida{
+    private HashMap<String, Double> precios = new HashMap<>();
     public Expreso() {
         descripcion="Expreso";
+        precios.put("N", 1.99);
+        precios.put("M", 2.09);
+        precios.put("G", 2.15);
     }
 
     @Override
     public double costo() {
-        return 1.99;
+        return precios.getOrDefault(getTamano(), 1.99);
     }
 }
