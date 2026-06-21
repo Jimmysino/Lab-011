1ae69f299d879ecc9500a8ebd7a8edbf8da1695e
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:06:33 -0500

    fix: Cambio en Batido para implementar HashMap

--- src/Batido.java
+++ src/Batido.java
@@ -1,10 +1,18 @@
+import java.util.HashMap;
+
 public class Batido extends  Bebida{
+    private HashMap<String, Double> precios = new HashMap<>();
     public Batido() {
         descripcion="Batido";
+        descripcion = "Batido";
+        precios.put("N", 0.89);
+        precios.put("M", 0.99);
+        precios.put("G", 1.09);
     }
 
     @Override
     public double costo() {
-        return 0.89;
+        return precios.getOrDefault(getTamano(), 0.89);
+
     }
 }
