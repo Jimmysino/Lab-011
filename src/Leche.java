9cab5df9cca4d35f892eaf5f7d062da0b106cea2
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:14:47 -0500

    fix: Cambio en Leche para implementar Hashmap

--- src/Leche.java
+++ src/Leche.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
+
 public class Leche extends Complemento{
-    Bebida cafe;
+    private HashMap<String, Double> precios = new HashMap<>();
 
     public Leche(Bebida cafe) {
         this.cafe = cafe;
+        precios.put("N", 0.10);
+        precios.put("M", 0.15);
+        precios.put("G", 0.20);
     }
 
     @Override
@@ -10,6 +15,6 @@ public class Leche extends Complemento{
         return cafe.getDescripcion()+", Leche";
     }
     public double costo(){
-        return 0.10+cafe.costo();
+        return cafe.costo() + precios.getOrDefault(getTamano(), 0.10);
     }
 }
