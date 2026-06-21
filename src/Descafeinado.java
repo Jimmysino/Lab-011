f943209b9a4759ceaf74466f7bdeb6da548d0455
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:07:40 -0500

    fix: Cambio en Descafeinado para implementar HashMap

--- src/Descafeinado.java
+++ src/Descafeinado.java
@@ -1,10 +1,16 @@
+import java.util.HashMap;
+
 public class Descafeinado extends Bebida{
+    private HashMap<String, Double> precios = new HashMap<>();
     public Descafeinado() {
-        descripcion="Descafeinado";
+        descripcion = "Descafeinado";
+        precios.put("N", 1.05);
+        precios.put("M", 1.15);
+        precios.put("G", 1.25);
     }
 
     @Override
     public double costo() {
-        return 1.05;
+        return precios.getOrDefault(getTamano(), 1.05);
     }
 }
