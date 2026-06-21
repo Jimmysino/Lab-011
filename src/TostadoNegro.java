57726d133013bd486706f622a247302c588a187a
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:04:51 -0500

    fix: Cambio en TostadoNegro para implementar HashMap

--- src/TostadoNegro.java
+++ src/TostadoNegro.java
@@ -1,12 +1,19 @@
-public class TostadoNegro extends Bebida {
+import java.util.HashMap;
 
+public class TostadoNegro extends Bebida {
+    private HashMap<String, Double> precios = new HashMap<>();
 
     public TostadoNegro() {
+
         descripcion="Tostado Negro";
+        descripcion = "Tostado Negro";
+        precios.put("N", 0.99);
+        precios.put("M", 1.09);
+        precios.put("G", 1.19);
     }
 
     @Override
     public double costo() {
-        return 0.99;
+       return precios.getOrDefault(getTamano(), 0.99);
     }
 }
