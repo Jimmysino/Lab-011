31fda760289cd3739f47229b12fec6a0a7ea8598
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:10:13 -0500

    fix: Cambio en Complemento para usar una inyeccion de bebida

--- src/Complemento.java
+++ src/Complemento.java
@@ -1,3 +1,8 @@
 public abstract class Complemento extends Bebida{
+    public Bebida cafe;
     public abstract String getDescripcion();
+    @Override
+    public String getTamano() {
+        return cafe.getTamano();
+    }
 }
