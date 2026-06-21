c66398672c7a05fe9358b601303ce91456f3acfc
Jimmy <leitotapia07@gmail.com>
2026-06-20 20:03:05 -0500

    fix: Cambio en bebida para implementar el tamaño

--- src/Bebida.java
+++ src/Bebida.java
@@ -1,9 +1,16 @@
 public abstract class Bebida {
     String descripcion="Bebida desconocida";
+    String tamano = "N";
 
     public abstract double costo();
 
     public String getDescripcion() {
         return descripcion;
+    }
+    public String getTamano() {
+        return tamano;
+    }
+    public void setTamano(String tamano) {
+        this.tamano = tamano.toUpperCase();
     }
 }
